d15659761bddd847120c806439f9bacc7440501d
Tsvetan Eftenov <105812168+eftenow@users.noreply.github.com>
2023-11-17 11:53:55 +0200

    added all the required company registration fields and added extra stylings to the register page

--- react/job-listing-app/client/src/Components/User/Register.tsx
+++ react/job-listing-app/client/src/Components/User/Register.tsx
@@ -1,6 +1,44 @@
-export const Register = () =>{
+export const Register = () => {
 
-    return(
-        <h1>register</h1>
+    return (
+        <section>
+            <h2 className="register-title">Register</h2>
+            <form className="register-form">
+                <label htmlFor="name" className="register-label">
+                    Company name:
+                </label>
+                <input type="text" id="name" name="name" className="register-input" />
+
+                <label htmlFor="email" className="register-label">
+                    Company email:
+                </label>
+                <input type="email" id="email" name="email" className="register-input" />
+
+                <label htmlFor="logo" className="register-label">
+                    Company logo:
+                </label>
+                <input type="text" id="logo" name="logo" className="register-input" />
+
+                <label htmlFor="password" className="register-label">
+                    Password:
+                </label>
+                <input type="password" id="password" name="password" className="register-input" />
+
+                <label htmlFor="rePassword" className="register-label">
+                    Repeat password:
+                </label>
+                <input type="password" id="rePassword" name="rePassword" className="register-input" />
+
+                <button type="submit" className="register-button" disabled>
+                    Create account
+                </button>
+                <p className="register-register-text">
+                    Already have an account?<br />
+                    <a href="/login" className="register-login-link">
+                        Login here
+                    </a>
+                </p>
+            </form>
+        </section>
     )
 }
